9be3a56f5b2314aaa1608a6f55a12175e004513a
chenqichen <chenqichen@kingsoft.com>
2021-11-29 18:33:05 +0800

    test v4

--- service/s3/service.go
+++ service/s3/service.go
@@ -5,7 +5,7 @@ package s3
 import (
 	"github.com/ks3sdklib/aws-sdk-go/aws"
 	"github.com/ks3sdklib/aws-sdk-go/internal/protocol/restxml"
-	"github.com/ks3sdklib/aws-sdk-go/internal/signer/v2"
+	"github.com/ks3sdklib/aws-sdk-go/internal/signer/v4"
 )
 
 // S3 is a client for Amazon S3.
@@ -29,7 +29,7 @@ func New(config *aws.Config) *S3 {
 	service.Initialize()
 
 	// Handlers
-	service.Handlers.Sign.PushBack(v2.Sign)
+	service.Handlers.Sign.PushBack(v4.Sign)
 	service.Handlers.Build.PushBack(restxml.Build)
 	//service.Handlers.Build.PushBack(aws.ContentTypeHandler)
 	service.Handlers.Unmarshal.PushBack(restxml.Unmarshal)
