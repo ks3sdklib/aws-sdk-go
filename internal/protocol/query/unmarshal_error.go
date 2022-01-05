d6748cd855dddc9b98d2e96d76f461680875519f
chenqichen <chenqichen@kingsoft.com>
2022-01-05 15:24:31 +0800

    fix UnmarshalError bug

--- internal/protocol/query/unmarshal_error.go
+++ internal/protocol/query/unmarshal_error.go
@@ -3,24 +3,35 @@ package query
 import (
 	"encoding/xml"
 	"io"
+	"io/ioutil"
+	"log"
 
 	"github.com/ks3sdklib/aws-sdk-go/aws"
 	"github.com/ks3sdklib/aws-sdk-go/internal/apierr"
 )
 
-type xmlErrorResponse struct {
-	XMLName   xml.Name `xml:"ErrorResponse"`
-	Code      string   `xml:"Error>Code"`
-	Message   string   `xml:"Error>Message"`
-	RequestID string   `xml:"RequestId"`
+type XmlErrorResponse struct {
+	XMLName    xml.Name `xml:"Error"`
+	Code       string   `xml:"Code"`
+	StatusCode int      `"StatusCode"`
+	Message    string   `xml:"Message"`
+	Resource   string   `xml:"Resource"`
+	RequestID  string   `xml:"RequestId"`
 }
 
 // UnmarshalError unmarshals an error response for an AWS Query service.
 func UnmarshalError(r *aws.Request) {
 	defer r.HTTPResponse.Body.Close()
 
-	resp := &xmlErrorResponse{}
-	err := xml.NewDecoder(r.HTTPResponse.Body).Decode(resp)
+	resp := &XmlErrorResponse{}
+	body, err := ioutil.ReadAll(r.HTTPResponse.Body)
+	if err != nil {
+		log.Printf("read body err, %v\n", err)
+		return
+	}
+	err = xml.Unmarshal(body, &resp)
+	resp.StatusCode = r.HTTPResponse.StatusCode
+
 	if err != nil && err != io.EOF {
 		r.Error = apierr.New("Unmarshal", "failed to decode query XML error response", err)
 	} else {
