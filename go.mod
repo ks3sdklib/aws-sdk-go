f6a7b5750f26b4b537e5186759d95f1d2b54b45d
cqc <cqclife@qq.com>
2022-09-05 16:49:01 +0800

    测试demo调整

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/ks3sdklib/aws-sdk-go
 go 1.16
 
 require (
-	github.com/KscSDK/ksc-sdk-go v0.1.42
 	github.com/aws/aws-sdk-go v1.42.27
 	github.com/deckarep/golang-set v1.8.0
 	github.com/stretchr/testify v1.7.0
+	gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405
 )
--- test/objectsample_test.go
+++ test/objectsample_test.go
@@ -1,96 +1,107 @@
-package ks3test
+package lib
 
 import (
-	"bufio"
 	"bytes"
-	"github.com/ks3sdklib/aws-sdk-go/aws/awserr"
-	"github.com/ks3sdklib/aws-sdk-go/aws/awsutil"
-	"net/url"
-	"strconv"
-	"strings"
-	"testing"
-	"time"
-	//	"io"
 	"fmt"
 	"github.com/ks3sdklib/aws-sdk-go/aws"
-	"github.com/ks3sdklib/aws-sdk-go/aws/credentials"
+	"github.com/ks3sdklib/aws-sdk-go/aws/awserr"
+	"github.com/ks3sdklib/aws-sdk-go/aws/awsutil"
 	"github.com/ks3sdklib/aws-sdk-go/service/s3"
+	. "gopkg.in/check.v1"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
-var bucket = string("yourbucket")
-var key = string("yourkey")
-var key_encode = string("yourkey")
-var key_copy = string("yourkey")
-var content = string("content")
-var prefix = "test/" //目录名称
-
-// 金山云主账号 AccessKey 拥有所有API的访问权限，风险很高。
-// 强烈建议您创建并使用子账号账号进行 API 访问或日常运维，请登录 https://uc.console.ksyun.com/pro/iam/#/user/list 创建子账号。
-// 通过指定 host(Endpoint)，您可以在指定的地域创建新的存储空间。
-var cre = credentials.NewStaticCredentials("ak", "sk", "") //online
-var svc = s3.New(&aws.Config{
-	//Region 可参考 https://docs.ksyun.com/documents/6761
-	Region:      "BEIJING",
-	Credentials: cre,
-	//Endpoint 可参考 https://docs.ksyun.com/documents/6761
-	Endpoint:         "ks3-cn-beijing.ksyuncs.com",
-	DisableSSL:       true, //是否禁用https
-	LogLevel:         1,    //是否开启日志,0为关闭日志，1为开启日志
-	LogHTTPBody:      true, //是否把HTTP请求body打入日志
-	S3ForcePathStyle: true,
-	Logger:           nil, //打日志的位置
-})
+var (
+	key      = randLowStr(10)
+	key_copy = randLowStr(10)
+)
 
 //列表bucket下对象
-func TestListObjects(t *testing.T) {
+func (s *Ks3utilCommandSuite) TestListObjects(c *C) {
 
-	objects1, _ := svc.ListObjects(&s3.ListObjectsInput{
+	objects1, _ := client.ListObjects(&s3.ListObjectsInput{
 		Bucket:    aws.String(bucket),
-		Delimiter: aws.String("/"),     //分隔符，用于对一组参数进行分割的字符
-		MaxKeys:   aws.Long(int64(30)), //设置响应体中返回的最大记录数（最后实际返回可能小于该值）。默认为1000。如果你想要的结果在1000条以后，你可以设定 marker 的值来调整起始位置。
-		Prefix:    aws.String(prefix),  //限定响应结果列表使用的前缀，正如你在电脑中使用的文件夹一样。
-		Marker:    aws.String(""),      //指定列举指定空间中对象的起始位置。KS3按照字母排序方式返回结果，将从给定的 marker 开始返回列表。
+		Delimiter: aws.String("/"),       //分隔符，用于对一组参数进行分割的字符
+		MaxKeys:   aws.Long(int64(1000)), //设置响应体中返回的最大记录数（最后实际返回可能小于该值）。默认为1000。如果你想要的结果在1000条以后，你可以设定 marker 的值来调整起始位置。
+		Prefix:    aws.String(""),        //限定响应结果列表使用的前缀，正如你在电脑中使用的文件夹一样。
+		Marker:    aws.String(""),        //指定列举指定空间中对象的起始位置。KS3按照字母排序方式返回结果，将从给定的 marker 开始返回列表。
 	})
 	//获取对象列表
 	objectList := objects1.Contents
-	for i := 0; i < len(objectList); i++ {
-		object := objectList[i]
-		println(*object.Key)
+	fmt.Println(objectList)
+}
+
+/**
+  上传示例 -可设置标签  acl
+*/
+func (s *Ks3utilCommandSuite) TestPutObject(c *C) {
+
+	//指定目标Object对象标签，可同时设置多个标签，如：TagA=A&TagB=B。
+	//说明 Key和Value需要先进行URL编码，如果某项没有“=”，则看作Value为空字符串。详情请见对象标签（https://docs.ksyun.com/documents/39576）。
+	v := url.Values{}
+	v.Add("name", "yz")
+	v.Add("age", "11")
+	XAmzTagging := v.Encode()
+
+	object := randLowStr(10)
+	s.createFile(object, content, c)
+	fd, _ := os.Open(content)
+	input := s3.PutObjectInput{
+		Bucket:      aws.String(bucket),
+		Key:         aws.String(object),
+		ACL:         aws.String("public-read"),
+		Body:        fd,
+		XAmzTagging: aws.String(XAmzTagging),
 	}
+	resp, _ := client.PutObject(&input)
+	fmt.Println("结果：\n", awsutil.StringValue(resp))
+	os.Remove(object)
+}
+
+/**
+  下载示例
+*/
+func (s *Ks3utilCommandSuite) TestGetObject(c *C) {
+
+	fd, _ := os.Open(content)
+	input := s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		ACL:    aws.String("public-read"),
+		Body:   fd,
+	}
+	resp, _ := client.PutObject(&input)
+	fmt.Println("结果：\n", awsutil.StringValue(resp))
+
+	//下载
+	getInput := s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	}
+	DownloadResp, _ := client.GetObject(&getInput)
+	fmt.Println("结果：\n", awsutil.StringValue(DownloadResp))
+
 }
 
 //删除对象
-func TestDelObject(svc *s3.S3) {
+func (s *Ks3utilCommandSuite) TestDelObject(c *C) {
 
-	resp, _ := svc.DeleteObject(&s3.DeleteObjectInput{
+	resp, _ := client.DeleteObject(&s3.DeleteObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 	})
 	fmt.Println("结果：\n", awsutil.StringValue(resp))
 }
 
-//下载文件
-func TestGetObject(svc *s3.S3) {
-
-	resp, _ := svc.GetObject(&s3.GetObjectInput{
-		Bucket:              aws.String(bucket),
-		Key:                 aws.String(key),
-		ResponseContentType: aws.String("application/pdf"), //响应类型
-		Range:               aws.String("bytes=0-1"),       //rang分块下载
-	})
-	//读取数据
-	br := bufio.NewReader(resp.Body)
-	//转成字符串
-	_, _ = br.ReadString('\n')
-	fmt.Println("结果：\n", awsutil.StringValue(resp))
-}
-
 //生成下载地址
-func TestGetObjectPresignedUrl(svc *s3.S3) {
+func (s *Ks3utilCommandSuite) TestGetObjectPresignedUrl(c *C) {
 
-	resp, _ := svc.GetObjectPresignedUrl(&s3.GetObjectInput{
+	resp, _ := client.GetObjectPresignedUrl(&s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 	}, time.Second*time.Duration(time.Now().Add(time.Second*600).Unix())) //在当前时间多久后到期
@@ -98,9 +109,9 @@ func TestGetObjectPresignedUrl(svc *s3.S3) {
 }
 
 //获取对象Acl
-func TestGetObjectAcl(svc *s3.S3) {
+func (s *Ks3utilCommandSuite) TestGetObjectAcl(c *C) {
 
-	resp, _ := svc.GetObjectACL(&s3.GetObjectACLInput{
+	resp, _ := client.GetObjectACL(&s3.GetObjectACLInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 	})
@@ -114,29 +125,29 @@ func TestGetObjectAcl(svc *s3.S3) {
 			foundRead = true
 		}
 	}
-	println(foundFull, foundRead)
+	fmt.Println(foundFull, foundRead)
 	fmt.Println("结果：\n", awsutil.StringValue(resp))
 
 }
 
 //设置对象Acl
-func TestPutObjectAcl(svc *s3.S3) {
+func (s *Ks3utilCommandSuite) TestPutObjectAcl(c *C) {
 
-	resp, _ := svc.PutObjectACL(&s3.PutObjectACLInput{
+	resp, _ := client.PutObjectACL(&s3.PutObjectACLInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
-		ACL:    aws.String("public-read"),
+		ACL:    aws.String("private"),
 	})
 	fmt.Println("结果：\n", awsutil.StringValue(resp))
 
 }
 
 //复制对象
-func TestCopyObject(svc *s3.S3) {
+func (s *Ks3utilCommandSuite) TestCopyObject(c *C) {
 
 	//设置对象Tag
 	v := url.Values{}
-	v.Add("schoole", "yz")
+	v.Add("school", "yz")
 	v.Add("class", "11")
 	XAmzTagging := v.Encode()
 
@@ -145,9 +156,9 @@ func TestCopyObject(svc *s3.S3) {
 	metadata["yourmetakey1"] = aws.String("yourmetavalue1")
 	metadata["yourmetakey2"] = aws.String("yourmetavalue2")
 
-	resp, _ := svc.CopyObject(&s3.CopyObjectInput{
+	resp, _ := client.CopyObject(&s3.CopyObjectInput{
 		Bucket:               aws.String(bucket),
-		Key:                  aws.String(key_copy),
+		Key:                  aws.String(key),
 		CopySource:           aws.String("/" + bucket + "/" + key_copy),
 		MetadataDirective:    aws.String("REPLACE"),
 		Metadata:             metadata,
@@ -158,7 +169,7 @@ func TestCopyObject(svc *s3.S3) {
 }
 
 //抓取第三方URL上传到KS3
-func fetchObj(svc *s3.S3) {
+func (s *Ks3utilCommandSuite) TestFetchObj(c *C) {
 
 	//指定目标Object对象标签，可同时设置多个标签，如：TagA=A&TagB=B。
 	//说明 Key和Value需要先进行URL编码，如果某项没有“=”，则看作Value为空字符串。详情请见对象标签（https://docs.ksyun.com/documents/39576）。
@@ -176,19 +187,19 @@ func fetchObj(svc *s3.S3) {
 		XAmzTagging: aws.String(XAmzTagging),   //对象tag
 		ACL:         aws.String("public-read"), //对象acl
 	}
-	resp, _ := svc.FetchObject(&input)
+	resp, _ := client.FetchObject(&input)
 	fmt.Println("结果：\n", awsutil.StringValue(resp))
 }
 
 //修改元数据信息
-func TestModifyObjectMeta(svc *s3.S3) {
+func (s *Ks3utilCommandSuite) TestModifyObjectMeta(c *C) {
 	key_modify_meta := string("yourkey")
 
 	metadata := make(map[string]*string)
 	metadata["yourmetakey1"] = aws.String("yourmetavalue1")
 	metadata["yourmetakey2"] = aws.String("yourmetavalue2")
 
-	resp, _ := svc.CopyObject(&s3.CopyObjectInput{
+	resp, _ := client.CopyObject(&s3.CopyObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key_modify_meta),
 		////空间名称与对象的object key名称的组合，通过斜杠分隔(’/’)。
@@ -209,11 +220,11 @@ func TestModifyObjectMeta(svc *s3.S3) {
 //关于请求签名的问题，分块上传为一系列的请求（初始化分块上传，上传块，完成分块上传，终止分块上传），用户启动任务，发送一个或多个分块，最终完成任务。用户需要对每一个请求单独签名。
 //
 //注意: 当你启动分块上传后，并开始上传分块，你必须完成或者放弃上传任务，才能终止因为存储造成的收费。
-func TestMultipartUpload(svc *s3.S3) {
+func (s *Ks3utilCommandSuite) TestMultipartUpload(c *C) {
 
 	key = "jdexe"
 	fileName := "d:/upload-test/jd.exe"
-	initRet, _ := svc.CreateMultipartUpload(&s3.CreateMultipartUploadInput{
+	initRet, _ := client.CreateMultipartUpload(&s3.CreateMultipartUploadInput{
 		Bucket:      aws.String(bucket),
 		Key:         aws.String(key),
 		ACL:         aws.String("public-read"),
@@ -234,10 +245,10 @@ func TestMultipartUpload(svc *s3.S3) {
 	//组装分块参数
 	compParts := []*s3.CompletedPart{}
 	partsNum := []int64{0}
-	s := make([]byte, 52428800)
+	sc := make([]byte, 52428800)
 
 	for {
-		nr, err := f.Read(s[:])
+		nr, err := f.Read(sc[:])
 		if nr < 0 {
 			fmt.Fprintf(os.Stderr, "cat: error reading: %s\n", err.Error())
 			os.Exit(1)
@@ -252,13 +263,13 @@ func TestMultipartUpload(svc *s3.S3) {
 			//块的数量可以是1到10,000中的任意一个（包含1和10,000）。块序号用于标识一个块以及其在对象创建时的位置。如果你上传一个新的块，使用之前已经使用的序列号，那么之前的那个块将会被覆盖。当所有块总大小大于5M时，除了最后一个块没有大小限制外，其余的块的大小均要求在5MB以上。当所有块总大小小于5M时，除了最后一个块没有大小限制外，其余的块的大小均要求在100K以上。如果不符合上述要求，会返回413状态码。
 			//
 			//为了保证数据在传输过程中没有损坏，请使用 Content-MD5 头部。当使用此头部时，KS3会自动计算出MD5，并根据用户提供的MD5进行校验，如果不匹配，将会返回错误信息。
-			resp, _ := svc.UploadPart(&s3.UploadPartInput{
+			resp, _ := client.UploadPart(&s3.UploadPartInput{
 				Bucket:        aws.String(bucket),
 				Key:           aws.String(key),
 				PartNumber:    aws.Long(i),
 				UploadID:      aws.String(uploadId),
-				Body:          bytes.NewReader(s[0:nr]),
-				ContentLength: aws.Long(int64(len(s[0:nr]))),
+				Body:          bytes.NewReader(sc[0:nr]),
+				ContentLength: aws.Long(int64(len(sc[0:nr]))),
 			})
 			partsNum = append(partsNum, i)
 			compParts = append(compParts, &s3.CompletedPart{PartNumber: &partsNum[i], ETag: resp.ETag})
@@ -272,7 +283,7 @@ func TestMultipartUpload(svc *s3.S3) {
 	//用户启动一个分块上传任务后，会使用 Upload Parts 接口上传所有的块。成功上传所有相关块之后，用户需要调用此接口来完成分块上传。收到完成请求后，KS3将会根据块序号将所有的块组装起来创建一个新的对象。在用户的完成任务请求中需要用户提供分块列表，由于KS3将会按照列表将所有块连接起来，所以要求用户保证所有的块已经完成上传。对于分块列表中的每一个块，用户需要在上传块时添加块序号以及对象的 ETag 头部，KS3则会在块完成上传后回复完成响应。
 	//
 	//请注意，如果 Complete Multipart Upload 请求失败了，用户应用应当能够进行重试操作。
-	compRet, _ := svc.CompleteMultipartUpload(&s3.CompleteMultipartUploadInput{
+	compRet, _ := client.CompleteMultipartUpload(&s3.CompleteMultipartUploadInput{
 		Bucket:   aws.String(bucket),
 		Key:      aws.String(key),
 		UploadID: aws.String(uploadId),
@@ -288,8 +299,8 @@ func TestMultipartUpload(svc *s3.S3) {
 //服务器端加密关乎静态数据加密，即 KS3 在将您的数据写入数据中心内的磁盘时会在对象级别上加密这些数据，并在您访问这些数据时为您解密这些数据。
 //只要您验证了您的请求并且拥有访问权限，您访问加密和未加密数据元的方式就没有区别。
 //例如，如果您使用预签名的 URL 来共享您的对象，那么对于加密和解密对象，该 URL 的工作方式是相同的。
-func TestPutObjectWithSSEC(svc *s3.S3) {
-	resp, _ := svc.PutObject(&s3.PutObjectInput{
+func (s *Ks3utilCommandSuite) TestPutObjectWithSSEC(c *C) {
+	resp, _ := client.PutObject(&s3.PutObjectInput{
 		Bucket:               aws.String(bucket),
 		Key:                  aws.String(key),
 		SSECustomerAlgorithm: aws.String("AES256"), //加密类型
@@ -299,7 +310,7 @@ func TestPutObjectWithSSEC(svc *s3.S3) {
 }
 
 //生成上传外链
-func TestPutObjectPresignedUrl(t *testing.T) {
+func (s *Ks3utilCommandSuite) TestPutObjectPresignedUrl(c *C) {
 	params := &s3.PutObjectInput{
 		Bucket:           aws.String(bucket),                    // bucket名称
 		Key:              aws.String(key),                       // object key
@@ -307,7 +318,7 @@ func TestPutObjectPresignedUrl(t *testing.T) {
 		ContentType:      aws.String("application/ocet-stream"), //设置文件的content-type
 		ContentMaxLength: aws.Long(20),                          //设置允许的最大长度，对应的header：x-amz-content-maxlength
 	}
-	resp, _ := svc.PutObjectPresignedUrl(params, 1444370289000000000) //第二个参数为外链过期时间，第二个参数为time.Duration类型
+	resp, _ := client.PutObjectPresignedUrl(params, 1444370289000000000) //第二个参数为外链过期时间，第二个参数为time.Duration类型
 	fmt.Println("结果：\n", awsutil.StringValue(resp))
 
 	//简单上传示例
@@ -322,33 +333,9 @@ func TestPutObjectPresignedUrl(t *testing.T) {
 	fmt.Println("结果：\n", awsutil.StringValue(upLoadResp))
 }
 
-//上传文件
-func putObj(svc *s3.S3) {
-
-	//获取本地文件FD
-	fd, _ := os.Open("D:\\suiyi.jpg")
-
-	//指定目标Object对象标签，可同时设置多个标签，如：TagA=A&TagB=B。
-	//说明 Key和Value需要先进行URL编码，如果某项没有“=”，则看作Value为空字符串。详情请见对象标签（https://docs.ksyun.com/documents/39576）。
-	v := url.Values{}
-	v.Add("name", "yz")
-	v.Add("age", "11")
-	XAmzTagging := v.Encode()
-
-	input := s3.PutObjectInput{
-		Bucket:      aws.String(bucket),
-		Key:         aws.String(key),
-		ACL:         aws.String("public-read"),
-		Body:        fd,
-		XAmzTagging: aws.String(XAmzTagging),
-	}
-	resp, _ := svc.PutObject(&input)
-	fmt.Println("结果：\n", awsutil.StringValue(resp))
-}
-
 //判断文件是否存在
 func objectExists(bucket, key string) bool {
-	_, err := svc.HeadObject(
+	_, err := client.HeadObject(
 		&s3.HeadObjectInput{
 			Bucket: &bucket,
 			Key:    &key,
@@ -367,7 +354,7 @@ func objectExists(bucket, key string) bool {
 /**
 批量删除对象
 */
-func DeleteObjects() {
+func (s *Ks3utilCommandSuite) DeleteObjects() {
 
 	params := &s3.DeleteObjectsInput{
 		Bucket: aws.String(""), // Required
@@ -383,7 +370,7 @@ func DeleteObjects() {
 			},
 		},
 	}
-	resp := svc.DeleteObjects(params)
+	resp := client.DeleteObjects(params)
 	fmt.Println("error keys:", resp.Errors)
 	fmt.Println("deleted keys:", resp.Deleted)
 }
@@ -391,13 +378,13 @@ func DeleteObjects() {
 /**
 删除前缀
 */
-func DeleteBucketPrefix(prefix string) {
+func (s *Ks3utilCommandSuite) DeleteBucketPrefix(prefix string) {
 
 	params := &s3.DeleteBucketPrefixInput{
 		Bucket: aws.String(""), // Required
 		Prefix: aws.String(prefix),
 	}
-	resp, _ := svc.DeleteBucketPrefix(params)
+	resp, _ := client.DeleteBucketPrefix(params)
 	fmt.Println("error keys:", resp.Errors)
 	fmt.Println("deleted keys:", resp.Deleted)
 }
@@ -405,13 +392,13 @@ func DeleteBucketPrefix(prefix string) {
 /**
 删除前缀(包含三次重试)
 */
-func TryDeleteBucketPrefix(prefix string) {
+func (s *Ks3utilCommandSuite) TryDeleteBucketPrefix(prefix string) {
 
 	params := &s3.DeleteBucketPrefixInput{
 		Bucket: aws.String(""),
 		Prefix: aws.String(prefix),
 	}
-	resp, _ := svc.TryDeleteBucketPrefix(params)
+	resp, _ := client.TryDeleteBucketPrefix(params)
 	fmt.Println("error keys:", resp.Errors)
 	fmt.Println("deleted keys:", resp.Deleted)
 }
@@ -423,7 +410,7 @@ func RestoreObject() {
 		Bucket: aws.String("ks3tools-test"),    // bucket名称
 		Key:    aws.String("/restore/big.txt"), // object key
 	}
-	resp, err := svc.RestoreObject(params)
+	resp, err := client.RestoreObject(params)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -432,13 +419,13 @@ func RestoreObject() {
 }
 
 //delObjectTagging
-func DelTag(svc *s3.S3) {
+func (s *Ks3utilCommandSuite) DelTag(c *C) {
 
 	params := &s3.DeleteObjectTaggingInput{
 		Bucket: aws.String(bucket), // Required
 		Key:    aws.String(key),
 	}
-	resp, err := svc.DeleteObjectTagging(params)
+	resp, err := client.DeleteObjectTagging(params)
 
 	if err != nil {
 		if awsErr, ok := err.(awserr.Error); ok {
@@ -455,13 +442,13 @@ func DelTag(svc *s3.S3) {
 }
 
 //getObjectTagging
-func GetTag(svc *s3.S3) {
+func (s *Ks3utilCommandSuite) GetTag(c *C) {
 
 	params := &s3.GetObjectTaggingInput{
 		Bucket: aws.String(bucket), // Required
 		Key:    aws.String(key),
 	}
-	resp, err := svc.GetObjectTagging(params)
+	resp, err := client.GetObjectTagging(params)
 
 	if err != nil {
 		if awsErr, ok := err.(awserr.Error); ok {
@@ -477,7 +464,7 @@ func GetTag(svc *s3.S3) {
 }
 
 //设置对象Tag
-func PutTag(svc *s3.S3) {
+func (s *Ks3utilCommandSuite) PutTag(c *C) {
 
 	//指定目标Object对象标签
 	objTagging := s3.Tagging{
@@ -495,6 +482,6 @@ func PutTag(svc *s3.S3) {
 		Key:     aws.String(key),
 		Tagging: &objTagging,
 	}
-	resp, _ := svc.PutObjectTagging(params)
+	resp, _ := client.PutObjectTagging(params)
 	fmt.Println("结果：\n", awsutil.StringValue(resp))
 }
